6f4cb9e20a0ec877dfd5c5e0462720a54c05109b
adnanCreativeDeveloper <156885803+adnanCreativeDeveloper@users.noreply.github.com>
2024-08-01 15:56:25 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import ChatbotLayout from "./components/ChatbotLayout";
+import Login from "./components/login_signup/Login";
+import SignUp from "./components/login_signup/SignUp";
+
+function App() {
+  return (
+    <>
+      {/* <Login/> */}
+      <SignUp/>
+    </>
+  );
+}
+
+export default App;
